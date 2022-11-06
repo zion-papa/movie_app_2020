ff030bb007e377e687b9634fdea80d73e386d547
jhlee <ljw881212@gmail.com>
2022-11-06 22:17:16 +0900

    ~영화앱클론코딩챕터08-2까지(Router에서 막힘)_2022-11-06

--- src/App.js
+++ src/App.js
@@ -1,54 +1,27 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
+//import Home from './routes/Home'
 import './App.css';
-//import PropTypes from "prop-types";
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
 
-
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    //ES6에서는 구조 분해 할당 권장
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false });
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section class="container">
-        {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">'Loading...'</span>
-          </div>
-        ) : (
-          <div class="movies">{
-            movies.map(movie => (
-              <Movie
-                key={movie.id}
-                title={movie.title}
-                year={movie.year}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        {/* //react 버전이 올라가면서 바뀜 component={About} -> elemnet={<About/>} */}
+        <Route path="/home">
+          <h1>Home</h1>
+        </Route>
+        <Route path="/home/introduction">
+          <h1>Introduction</h1>
+        </Route>
+        <Route path="/about">
+          <h1>About</h1>
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
